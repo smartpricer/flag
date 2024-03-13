5c8605cb0a17a993a768edfd8ac8b3d833aad1d7
Ferris Schwanebeck <accounts@ferris-s.de>
2024-03-13 21:23:30 +0100

    improved update mechnism

--- Makefile
+++ Makefile
@@ -6,8 +6,5 @@ include go.mk
 all: update
 
 update:
-	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/example_test.go
-	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/example_value_test.go
-	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/export_test.go
-	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/flag.go
-	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/flag_test.go
+	./update_flag.sh
+
---
+++ update_flag.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clean=$(git status | grep "nothing to commit (working directory clean)")
+if [ -z "$clean" ]; then
+    echo There are uncommitted changes.
+    echo Updating will overwrite, commit or stash first.
+    exit 1
+else
+    echo You might want to check if there are new files upstream:
+    echo https://github.com/golang/go/tree/master/src/flag
+    echo 
+    curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/example_func_test.go
+	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/example_textvar_test.go
+	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/example_test.go
+	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/example_value_test.go
+	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/export_test.go
+	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/flag.go
+	curl -LO https://raw.githubusercontent.com/golang/go/master/src/flag/flag_test.go
+
+fi
+
